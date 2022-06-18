10b534111e697dfafc97c1313d3214236c39b444
dag-bag <virenderkumar23435@gmail.com>
2022-06-18 20:48:15 +0530

    main

--- components/admin/Sidebar.js
+++ components/admin/Sidebar.js
@@ -220,11 +220,11 @@ export default function Sidebar() {
           </nav>
 
           <div className="flex items-center px-4 -mx-2">
-            <img
+            {/* <img
               className="object-cover mx-2 rounded-full h-9 w-9"
               src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
               alt="avatar"
-            />
+            /> */}
             <h4 className="mx-2 font-medium text-gray-800  hover:underline">
               John Doe
             </h4>
